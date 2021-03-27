43ed544686bfab2d2666281c6437217ca891cae6
Atrin Hojjat <atrin.hojjat@gmail.com>
2021-03-27 16:13:04 +0430

    remove LC_COLLATE

--- back/PG/DOM_DB.sql
+++ back/PG/DOM_DB.sql
@@ -1,24 +1,17 @@
 -- Database: DOMessenger
+ -- DROP DATABASE "DOMessenger";
 
--- DROP DATABASE "DOMessenger";
+CREATE DATABASE dom WITH ENCODING = 'UTF8' --  LC_COLLATE = 'C'
+ --  LC_CTYPE = 'C'
+ TABLESPACE = pg_default CONNECTION
+LIMIT = -1;
 
-CREATE DATABASE "DOM"
-    WITH 
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
 --user DOMessenger
-\connect DOM
-create table if not exists users ( 
-	username varchar(32) NOT NULL primary key,
-	password varchar NOT NULL);
-create table if not exists chats ( 
-	name varchar(32) NOT NULL,
-	chat_id SERIAL PRIMARY KEY);
-	
-create table if not exists chat_user (
-		chat_id serial,
-		username varchar(32) not null
-	);
+\connect dom
+create table if not exists users (username varchar(32) NOT NULL primary key, password varchar NOT NULL);
+
+
+create table if not exists chats (name varchar(32) NOT NULL, chat_id SERIAL PRIMARY KEY);
+
+
+create table if not exists chat_user (chat_id serial, username varchar(32) not null);
